749587bcfa645c8b603382100995e464b89b727f
BrettForbes <brett@cloudaccelerator.co>
2023-08-20 19:24:20 +1000

    Setup Get Object Block

--- General/Get_Object/script.py
+++ General/Get_Object/script.py
@@ -21,6 +21,9 @@ import copy
 import os
 import sys
 import argparse
+from stixorm.module.orm.export_object import convert_ans_to_stix
+from stixorm.module.typedb import get_embedded_match
+from stixorm.module.typedb_lib.queries import delete_database, match_query
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
@@ -37,13 +40,37 @@ connection = {
 }
 report_id = "report--f2b63e80-b523-4747-a069-35c002c690db"
 
+
+def __retrieve_stix_dict(stix_id: str):
+    logger.debug(f'__retrieve_stix_object: {stix_id}')
+    obj_var, type_ql = get_embedded_match(stix_id, import_type)
+    query = 'match ' + type_ql
+    logger.debug(f'query is {query}')
+
+    stix_dict = match_query(uri=connection["uri"],
+                       port=connection["port"],
+                       database=connection["database"],
+                       query=query,
+                       data_query=convert_ans_to_stix,
+                       import_type=import_type)
+
+    logger.debug(f'stix_dict is -> {stix_dict}')
+
+    # result = write_to_file("stixorm/module/orm/export_final.json", stix_obj)
+    # if not is_successful(result):
+    #     logging.exception("\n".join(traceback.format_exception(result.failure())))
+    #     logger.error(str(result.failure()))
+
+    return stix_dict
+
+
 def get_object(object_id, connection):
     object_type = object_id.split('--')[0]
 
     typedb_source = TypeDBSource(connection, import_type)
-    stix_object = typedb_source.get(object_id)
+    stix_dict = __retrieve_stix_dict(object_id)
 
-    return stix_object
+    return stix_dict
 
 
 def main(dbhost, dbport, dbdatabase, dbquery, outputfile, logger: Logger):
@@ -56,10 +83,9 @@ def main(dbhost, dbport, dbdatabase, dbquery, outputfile, logger: Logger):
     }
     # setup logger for execution
     report_id = dbquery
-    stix_object = get_object(report_id, instance_connection)
-    stix_dict = stix_object.serialize,
+    stix_dict = get_object(report_id, instance_connection)
     print(f"\n type of stix_dict is {type(stix_dict)}")
-    print(stix_object.serialize(pretty=True))
+    print(stix_dict)
     with open(outputfile, "w") as outfile:
         json.dump(stix_dict, outfile)
 
